9c85c14d174883ed526a0f4ea852481a04a1ca23
Linky Linkerson <aldovibanco85@gmail.com>
2020-06-08 18:13:19 -0700

    adding new css to make it responsive

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import './style.css'
 
 function Footer () {
 
     return (
-        <footer className="mt-5">
-            <Container fluid={true} className='footer'>
+        <footer className="footer mt-5">
                 <Row className="border-top justify-content-center p-3">
-                    <Col className="p-0" md={3} sm={12}>
-                      Black Jack by Aldo Vibanco and Thanh Ngo, Copyright &copy; 2020
-                    </Col>
+                      Black Jack by Aldo Vibanco and Thanh Ngo, Copyright &copy; 2020 
                 </Row>
-            </Container>
-
         </footer>
     )
         
