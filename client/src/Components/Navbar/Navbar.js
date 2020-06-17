f9fa3f859e508a8d0263e726e6998c33b9211816
thanhn <510thanh.ngo@gmail.com>
2020-06-17 14:28:29 -0700

    test

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -17,12 +17,14 @@ function NavBar(props) {
       console.log(error);
     });
     return (
+        console.log(response.data.email);
         <Navbar fixed="top" className="Navbar" bg="light" expand="lg">
             <Navbar.Brand>Welcome!</Navbar.Brand>
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
                 <Navbar.Collapse id="navbar-toggle">
                     <Nav className="ml-auto">
                         <Link className="nav-link" style={{color:"black"}} to="/">Home</Link>
+                        <Link className={response.data.email ? "nav-link" : "nav-link disabled"} style={{color:"black"}} to="/play">Play</Link>
                         <Link className="nav-link" style={{color:"black"}} to="/login">Login</Link>
                         <Link className="nav-link" style={{color:"black"}} to="/signup">Sign Up</Link>
                         {/* <Link className="nav-link" style={{color:"black"}} to="/leaderboards">Leaderboards</Link> */}
