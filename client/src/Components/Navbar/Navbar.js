137f4c3994eaec5f95574de20d1efdb88eaaaa53
thanhn <510thanh.ngo@gmail.com>
2020-06-17 17:17:32 -0700

    Undefined

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -25,7 +25,7 @@ function NavBar(props) {
                 <Navbar.Collapse id="navbar-toggle">
                     <Nav className="ml-auto">
                         <Link className="nav-link" style={{color:"black"}} to="/">Home</Link>
-                        <Link className={email ? "nav-link show" : "nav-link hide"} style={{color:"black"}} to="/play">Play</Link>
+                        <Link className={email !== "undefined" ? "nav-link show" : "nav-link hide"} style={{color:"black"}} to="/play">Play</Link>
                         <Link className="nav-link" style={{color:"black"}} to="/login">Login</Link>
                         <Link className="nav-link" style={{color:"black"}} to="/signup">Sign Up</Link>
                         {/* <Link className="nav-link" style={{color:"black"}} to="/leaderboards">Leaderboards</Link> */}
